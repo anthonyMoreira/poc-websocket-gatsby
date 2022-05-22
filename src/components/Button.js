e342872866a425d084f5275daee28834fb7e286d
anthony.moreira <anthony.moreira@webbfontaine.com>
2022-05-22 22:25:42 +0200

    Initial commit

---
+++ src/components/Button.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  position: relative;
+  width: 180px;
+  height: 60px;
+  margin: 20px;
+  line-height: 60px;
+  letter-spacing: 2px;
+  text-decoration: none;
+  text-transform: uppercase;
+  text-align: center;
+  color: black;
+`;
+
+export default Button;
